75b196596e6f1f3d83e7767883fe411bf134b641
Matthieu Vilain <matthieuvilain@MacBook-Pro-de-Matthieu.local>
2022-08-29 17:59:58 -0400

    onehot goes through numpy (now faster than python), also add random_seq and MakeKmer function (faster than python)

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,60 @@
-
-
 pub mod onehot;
 pub mod kmers;
 pub mod random_sequence;
-pub mod fasta_files;
+
+
+
+use numpy::{IntoPyArray, PyArray, PyArray2};
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+use numpy::ndarray::Array2;
+use numpy::ndarray::ArrayView;
+use pyo3::types::PyModule;
+use pyo3::PyResult;
+
+use onehot::one_hot;
+use kmers::make_kmers;
+use random_sequence::random_seq;
+
+
+#[pyfunction]
+fn OneHot<'pyt>(py:  Python <'pyt>, seq: String) ->  &'pyt PyArray2<i8>{
+
+
+    let  matrix= one_hot(&seq);
+    let pyarray= matrix.into_pyarray(py);
+
+    return pyarray
+    
+}
+
+
+#[pyfunction]
+fn MakeKmers(seq: String, k: i64) -> String {
+
+    let return_seq= make_kmers(k,&seq);
+
+    return return_seq
+}
+
+#[pyfunction]
+fn RandomSeq(length: i64, seq_type: String) -> String {
+
+    let return_seq= random_seq(length, &seq_type);
+    return return_seq
+}
+
+#[pymodule]
+fn dna_parser(_py: Python<'_>, m: &PyModule)-> PyResult<()> {
+
+    m.add_function(wrap_pyfunction!(OneHot,m)?)?;
+    m.add_function(wrap_pyfunction!(MakeKmers,m)?)?;
+    m.add_function(wrap_pyfunction!(RandomSeq,m)?)?;
+
+
+    Ok(())
+}
+
 
 
 #[cfg(test)]
--- src/onehot.rs
+++ src/onehot.rs
@@ -1,33 +1,24 @@
-pub fn one_hot(sequence: &str)-> Vec<Vec<i32>> {
+use numpy::ndarray::{Array, ArrayView, array};
+use numpy::ndarray::ArrayBase;
+use numpy::ndarray::Array2;
+
+pub fn one_hot(sequence: &str)-> Array2<i8> {
 
     let seq= sequence.to_lowercase();
-    let mut vec= Vec::new();
+    let mut matrix= Array2::<i8>::zeros((0,4));
 
     //define values for one hot encoding
-
     for charac in seq.chars() {
 
-        if charac== 'a' {
+        match charac {
 
-            vec.push(vec![1,0,0,0]);
+            'a' => matrix.push_row(ArrayView::from(&[ 1,  0,  0,  0])).unwrap(),
+            'c' => matrix.push_row(ArrayView::from(&[ 0,  1,  0,  0])).unwrap(),
+            'g' => matrix.push_row(ArrayView::from(&[ 0,  0,  1,  0])).unwrap(),
+            't' => matrix.push_row(ArrayView::from(&[ 0,  0,  0,  1])).unwrap(),
+            _ => continue
         }
-
-        else if charac== 'c'{
-
-            vec.push(vec![0,1,0,0]);
-        }
-
-        else if charac== 'g'{
-
-            vec.push(vec![0,0,1,0]);
-        }
-
-        else if charac== 't'{
-
-            vec.push(vec![0,0,0,1]);
-        }
-
     }
-    
-    return vec
+    return matrix
 }
+
